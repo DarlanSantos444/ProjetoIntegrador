000467b8bbf3ba3319d29bd3a6d8a1049146f8a1
DarlanSantos444 <darlan.santos.10420@gmail.com>
2020-10-02 11:35:26 -0300

    Inicio

---
+++ src/projeto/Projeto.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto;
+
+/**
+ *
+ * @author Alzilene Alencar
+ */
+public class Projeto {
+
+    String tipo;
+    double diametro;
+    double peso_metro;
+    int quantidade;
+    int comprimento;
+    double peso_ferro = quantidade * comprimento * peso_metro;
+    double comp_total = quantidade * comprimento;
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

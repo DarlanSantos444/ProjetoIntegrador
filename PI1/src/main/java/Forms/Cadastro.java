1058541329aa9101548d20b792a1bfd67d67be6c
DarlanSantos444 <darlan.santos.10420@gmail.com>
2020-10-29 19:23:04 -0300

    Login

--- PI1/src/main/java/Forms/Cadastro.java
+++ PI1/src/main/java/Forms/Cadastro.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Forms;
 
-/**
- *
- * @author Alzilene Alencar
- */
 public class Cadastro extends javax.swing.JFrame {
 
     /**
